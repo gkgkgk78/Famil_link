c30ce2145efcd5062625267c9fa2c22e2fd8f19e
기남석 <kee0304@naver.com>
2023-01-24 19:32:05 +0900

    [FE] feat 기본 레이아웃 추가

--- RM/MW/mirrorweb/src/App.js
+++ RM/MW/mirrorweb/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Clock from './modules/Clock';
+
+import FaceValid from './pages/FaceValid';
+import PlayVideo from './pages/PlayVideo';
+import Main from './pages/Main';
+import Record from './pages/Record';
+
+import { Box } from '@mui/system';
+
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
+
+  // 라즈베리파이와 웹소켓을 연결되어, 신호를 주고받는다.
+  // 초음파 센서 신호가 오면 안면 인식 화면으로 이동한다.
+  // 초음파 센서 신호가 사라지면 메인 페이지로 돌아가는데, 어차피 디스플레이는 꺼진다.
+  // App.js에서 제스쳐나 음성 신호 등이 들어오면 영상 녹화 화면으로 이동한다.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+    className="App"
+    >
+      <Box sx={{
+        display: "flex",
+        justifyContent:"space-between",
+        p: 3
+      }}>
+        <Clock />
+        <div>날씨</div>
+      </Box>
+      
+      <div>
+        <Routes>
+          <Route path="/" element= { <Main />} />
+          <Route path="/facevalid" element={ <FaceValid /> } />
+          <Route path="/playvideo" element={ <PlayVideo /> } />
+          <Route path="/record" element={<Record/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
