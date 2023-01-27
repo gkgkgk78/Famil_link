341bea129d2026c75c9c78b123b8b47fa9aa5eda
woojin <woojin1home@naver.com>
2023-01-27 17:06:47 +0900

    [BE] fix : 수정수정

--- BE/Spring/src/main/java/com/famillink/util/JwtTokenProvider.java
+++ BE/Spring/src/main/java/com/famillink/util/JwtTokenProvider.java
@@ -3,6 +3,7 @@ package com.famillink.util;
 import com.famillink.exception.BaseException;
 import com.famillink.exception.ErrorMessage;
 import com.famillink.model.mapper.AccountMapper;
+import com.famillink.model.mapper.MemberMapper;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.*;
@@ -46,6 +47,7 @@ public class JwtTokenProvider {
     private long refreshValidMinutes;
 
     private final AccountMapper accountMapper;
+    private final MemberMapper memberMapper;
 
 
     @PostConstruct
@@ -56,7 +58,7 @@ public class JwtTokenProvider {
 
     //해당 부분은 토큰을 생성하는 부분입니다.
     public String create(Long uid, List<String> roles, long expire) {
-        Claims claims = Jwts.claims().setSubject(Long.toString(uid));//jwt의 토큰의 내용에 값을 넣기 위해 claims객체를 생성을 합니다., 
+        Claims claims = Jwts.claims().setSubject(Long.toString(uid));//jwt의 토큰의 내용에 값을 넣기 위해 claims객체를 생성을 합니다.,
         // setsubject 메서드를 통하여 sub속성에 값을 추가하고자 할시에 User의 uid를 사용합니다
         claims.put("roles", roles);//해당 부분은 해당 토큰을 사용하는 사용자의 권한을 확인 할수 있는 role값을 추가한 부분입니다.
         Date now = new Date();
@@ -69,9 +71,26 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    public String create(Long uid, List<String> roles, long expire,int flag) {
+        Claims claims = Jwts.claims().setSubject(Long.toString(uid));//jwt의 토큰의 내용에 값을 넣기 위해 claims객체를 생성을 합니다.,
+        // setsubject 메서드를 통하여 sub속성에 값을 추가하고자 할시에 User의 uid를 사용합니다
+        claims.put("roles", roles);//해당 부분은 해당 토큰을 사용하는 사용자의 권한을 확인 할수 있는 role값을 추가한 부분입니다.
+        Date now = new Date();
+
+        return Jwts.builder()//Jwts.builder를통해서 토큰을 생성합니다.
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expire)) // 토큰 만료일 설정
+                .signWith(SignatureAlgorithm.HS256, (secretKey + memberMapper.getSalt(uid)).getBytes()) // 암호화
+                .compact();
+    }
+
     // JWT 토큰 생성
     public String createToken(Long uid, List<String> roles) {
 //        return create(uid, roles, 1000 * 5);
+
+
+
         return create(uid, roles, 1000 * 10 * tokenValidMinutes);
     }
 
@@ -100,6 +119,16 @@ public class JwtTokenProvider {
                 .getSubject();
     }
 
+    public String getUserId(String token,int flag) {
+        return Jwts.parser()//jwt parser를 통해 secretkey를 설정하고 클레임을 추출해서 토큰을 생성할시 넣었던 sub값을 추출합니다.
+                .setSigningKey((secretKey + memberMapper.getSalt(getUid(token))).getBytes())
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
+
     // Request header에 Authorization 의 값에서 token 꺼내옴
     //즉 클라이언트가 헤더를 통해 jwt토큰값을 제대로 전달 했는지 파악 가능한 메서드
     public String resolveToken(HttpServletRequest request) {
@@ -118,6 +147,22 @@ public class JwtTokenProvider {
     public boolean validateToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser().setSigningKey((secretKey + accountMapper.getSalt(getUid(token))).getBytes(StandardCharsets.UTF_8)).parseClaimsJws(token);
+
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (SecurityException | MalformedJwtException | IllegalArgumentException exception) {
+            logger.info("잘못된 Jwt 토큰입니다");
+        } catch (ExpiredJwtException exception) {
+            logger.info("만료된 Jwt 토큰입니다");
+        } catch (UnsupportedJwtException exception) {
+            logger.info("지원하지 않는 Jwt 토큰입니다");
+        }
+
+        return false;
+    }
+
+    public boolean validateToken(String token,int flag) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey((secretKey + memberMapper.getSalt(getUid(token))).getBytes(StandardCharsets.UTF_8)).parseClaimsJws(token);
 
             return !claims.getBody().getExpiration().before(new Date());
         } catch (SecurityException | MalformedJwtException | IllegalArgumentException exception) {
