2cddbbb27abb118c4fae608b9170f42799084a26
DESKTOP-DOGVPUB\SSAFY <cjw.git@gmail.com>
2023-01-31 15:10:24 +0900

    [BE] test : mp4 streaming test

--- BE/Spring/src/main/java/com/famillink/controller/TestController.java
+++ BE/Spring/src/main/java/com/famillink/controller/TestController.java
@@ -1,41 +1,38 @@
 package com.famillink.controller;
 
-import com.famillink.exception.BaseException;
-import com.famillink.exception.ErrorMessage;
 import com.famillink.model.service.TestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.file.Files;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
-@RestController
+
 @Api("Test Controller")
 @RequiredArgsConstructor
+@RestController
+@CrossOrigin("*")
 public class TestController {
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    @Autowired
-    private TestService testService;
+    private final TestService testService;
 
     @ApiOperation(value = "테스트", notes = "테스트 컨트롤러입니다.")
     @GetMapping("/test")
@@ -65,49 +62,22 @@ public class TestController {
     }
 
     @ApiOperation(value = "동영상 스트리밍")
-    @GetMapping("/straming")
-    public StreamingResponseBody movieStreaming(final Authentication authentication) throws Exception {
+    @GetMapping(path = "/streaming")
+    public ResponseEntity<StreamingResponseBody> movieStreaming(@RequestHeader HttpHeaders headers) throws Exception {
+//        return new InputStreamResource(new FileInputStream("upfiles\\record.mp4"));
+        // ResponseEntity<StreamingResponseBody>
         File file = new File("upfiles\\record.mp4");
         if (!file.isFile()) {
-            throw new BaseException(ErrorMessage.NOT_EXIST_ROUTE);
+            return ResponseEntity.notFound().build();
         }
 
-        StreamingResponseBody streamingResponseBody = new StreamingResponseBody() {
-            @Override
-            public void writeTo(OutputStream outputStream) throws IOException {
-                try {
-                    final InputStream inputStream = new FileInputStream(file);
-
-                    byte[] bytes = new byte[1024];
-                    int length;
-                    while ((length = inputStream.read(bytes)) >= 0) {
-                        outputStream.write(bytes, 0, length);
-                    }
-                    inputStream.close();
-                    outputStream.flush();
-
-                } catch (final Exception e) {
-                    System.out.println(e);
-                }
-            }
+        StreamingResponseBody streamingResponseBody = outputStream -> {
+            FileCopyUtils.copy(Files.newInputStream(file.toPath()), outputStream);
+            System.out.println("dd");
         };
-
         final HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Content-Type", "video/mp4");
         responseHeaders.add("Content-Length", Long.toString(file.length()));
-
-        final InputStream is = Files.newInputStream(file.toPath());
-        return os -> {
-            readAndWrite(is, os);
-        };
-    }
-
-    private void readAndWrite(final InputStream is, OutputStream os) throws IOException {
-        byte[] data = new byte[2048];
-        int read = 0;
-        while ((read = is.read(data)) > 0) {
-            os.write(data, 0, read);
-        }
-        os.flush();
+        return ResponseEntity.ok().headers(responseHeaders).body(streamingResponseBody);
     }
 }
