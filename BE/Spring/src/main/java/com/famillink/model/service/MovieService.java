923cf2a2af498e38efc36a363afddd56967f56e5
DESKTOP-DOGVPUB\SSAFY <cjw.git@gmail.com>
2023-02-01 16:55:09 +0900

    [BE] feat : 동영상 스트리밍

--- BE/Spring/src/main/java/com/famillink/model/service/MovieService.java
+++ BE/Spring/src/main/java/com/famillink/model/service/MovieService.java
@@ -2,19 +2,18 @@ package com.famillink.model.service;
 
 import com.famillink.model.domain.param.MovieSenderDTO;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 public interface MovieService {
 
 
     void sender(MovieSenderDTO sender, MultipartFile file) throws Exception;
 
-    InputStreamResource download(Long movie_uid) throws Exception;
+    StreamingResponseBody download(Long movie_uid, HttpHeaders httpHeaders) throws Exception;
 
 
     void setRead(Long movie_uid)throws Exception;
-
-
-
 
 }
--- BE/Spring/src/main/java/com/famillink/model/service/MovieServiceImpl.java
+++ BE/Spring/src/main/java/com/famillink/model/service/MovieServiceImpl.java
@@ -10,10 +10,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.File;
+import java.nio.file.Files;
 
 
 @Service
@@ -53,7 +60,7 @@ public class MovieServiceImpl implements MovieService {
     }
 
     @Override
-    public InputStreamResource download(Long movie_uid) throws Exception {
+    public StreamingResponseBody download(Long movie_uid, HttpHeaders httpHeaders) throws Exception {
 
         try {
             //Movie table에서 보낸자와 받은 자가 같은 가족인지 판단을 해서 처리를 함
@@ -68,22 +75,27 @@ public class MovieServiceImpl implements MovieService {
         }
         String filename = movieMapper.getMoviePath(movie_uid);
         filename = "./" + filename;
+        File file = new File(filename);
+        if (!file.isFile()) {
+            throw new BaseException(ErrorMessage.NOT_READ_FILE);
+        }
+        httpHeaders.add("Content-Length", Long.toString(file.length()));
+
         // 파일 리소스 리턴
-        return fileService.loadAsResource(filename);
+        return outputStream -> {
+            FileCopyUtils.copy(Files.newInputStream(file.toPath()), outputStream);
+        };
     }
 
 
     @Override
     public void setRead(Long movie_uid) throws Exception {
 
-        try{
+        try {
             movieMapper.setMovie(movie_uid);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new BaseException(ErrorMessage.NOT_READ_FILE);
         }
-
 
 
     }
