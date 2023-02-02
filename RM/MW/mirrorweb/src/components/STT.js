34bb9e06640d9e9003d7bcdc612cf7afcdeff67d
기남석 <kee0304@naver.com>
2023-02-03 01:05:20 +0900

    [MW] refactor backup

--- RM/MW/mirrorweb/src/components/STT.js
+++ RM/MW/mirrorweb/src/components/STT.js
@@ -1,15 +1,59 @@
-import React, {useState} from 'react';
+import { useEffect, useRef, useState } from "react";
+import secrets from "../secrets.json"
 
-function STT() {
+import useSpeechToText from "react-hook-speech-to-text"
+import { useNavigate, useLocation } from "react-router-dom";
 
+const STT = () => {
     const API_KEY = secrets.google_speech_api_key
-    const ttsURL = `https://texttospeech.googleapis.com/v1/text:synthesize?key=${API_KEY}`
+    const mounted = useRef(false);
 
-    return (
-        <div>
-            
-        </div>
-    );
-}
+    const {
+        error,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+      } = useSpeechToText({
+        continuous: true,
+        useLegacyResults: false,
+        crossBrowser: true,
+        useOnlyGoogleCloud: true,
+        googleApiKey: API_KEY,
+        googleCloudRecognitionConfig: {
+            languageCode: 'ko-KR'
+          }
+      });
+
+    const Navigate = useNavigate();
+    const location = useLocation();
+    
+    
+    setTimeout(() => {
+        startSpeechToText()
+    }, 50)
+
+    useEffect(() => {
+        if (!mounted.current) {
+            mounted.current = true;
+        } else{
+            if (results.length>1){
+                if (location.pathname !== "/record") {
+                    if (text.includes("녹화") || text.includes("노콰"))   { 
+                          Navigate("/record")
+                      }
+                  } else if (location.pathname === "/record") {
+                    console.log("")
+                  }
+
+            }
+        }
+      },[results])
+
+      if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+    }
+
+
 
 export default STT;
