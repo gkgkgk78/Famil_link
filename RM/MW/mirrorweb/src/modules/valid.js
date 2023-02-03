46bf917f78b04336c2357cebbaced96a126e8b4c
기남석 <kee0304@naver.com>
2023-02-03 12:50:01 +0900

    [MW] feat redux factoring

--- RM/MW/mirrorweb/src/modules/valid.js
+++ RM/MW/mirrorweb/src/modules/valid.js
@@ -1,18 +1,21 @@
 // 액션 타입
-
 const CHANGE_VALID = "valid/CHANGE_VALID"
 const SET_INFO = "valid/SET_INFO"
+const START_RECORDING = "valid/START_RECORDING"
+const STOP_RECORDING = "valid/STOP_RECORDING"
 
 
 // 액션 생성 함수
-
 export const setValid = () => ({type:CHANGE_VALID});
 export const setInfo = (info) => ({type: SET_INFO, info})
+export const startRecording = () => ({type: START_RECORDING})
+export const stopRecording = () => ({type: STOP_RECORDING})
 
 
 // 초기 상태
 const initialState = {
     validation: false,
+    isRecording: false,
     memberInfo: {}
 }
 
@@ -29,6 +32,16 @@ export default function valid(state = initialState, action) {
             return {
                 ...state,
                 memberInfo: action.info
+            }
+        case START_RECORDING:
+            return {
+                ...state,
+                isRecording: true
+            }
+        case STOP_RECORDING:
+            return {
+                ...state,
+                isRecording: false
             }
     }
 }
--- RM/MW/mirrorweb/src/pages/Record.js
+++ RM/MW/mirrorweb/src/pages/Record.js
@@ -1,8 +1,36 @@
+import {useSelector,useDispatch} from "react-redux"
+import { startRecording, stopRecording } from "../modules/valid";
+import React, {useRef} from 'react';
+
+
 const Record = () => {
-    // 영상 녹화 중엔 따로 표시해줄 화면은 없다.
+    // redux에서 불러오기
+    const { memberInfo, recording } = useSelector(state => ({
+        recording: state.valid.isRecording
+    }))
+    // action dispatch
+    const dispatch = useDispatch();
+    const onRecording = () => dispatch(startRecording())
+    const notRecording = () => dispatch(stopRecording())
+
+    const timer = useRef(0);
+
+    // 들어오고 1초뒤에 녹화 시작
+    setTimeout( () => {
+        onRecording()
+        setInterval(() => {
+           timer.current +=1 
+        }, 1000);
+    }, 1000)
+
+    // 녹화 시작하고 30초 뒤에 자동으로 녹화 종료
+    setTimeout( () => {
+        notRecording()
+    }, 31000)
+
     return ( 
         <div>
-            123123
+            {timer.current}
         </div>
      );
 }
