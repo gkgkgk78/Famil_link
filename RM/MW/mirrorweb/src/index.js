4d3898555df7caf5f569ee15e8312c687b42ef83
기남석 <kee0304@naver.com>
2023-02-03 13:27:58 +0900

    [MW] test reduxtest

--- RM/MW/mirrorweb/src/index.js
+++ RM/MW/mirrorweb/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
 
 // redux
+
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 // router
 import { BrowserRouter } from 'react-router-dom';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
