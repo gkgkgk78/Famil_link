a4353aba3d65561ec6a7d58436db80bca3b09c52
최다혜 <dhc0404@naver.com>
2023-02-08 16:58:26 +0900

    [BE] feat : SSE 영상 전송 시 이벤트 발생 기능 구현

--- BE/Spring/src/main/java/com/famillink/model/service/SseService.java
+++ BE/Spring/src/main/java/com/famillink/model/service/SseService.java
@@ -15,10 +15,10 @@ public interface SseService {
 
     SseEmitter subscribe(Long member_to, String lastEventId); //구독
 
-    //다른 사용자가 알림을 보내는 기능 (영상 전송 버튼 누를 시)
+    //다른 사용자가 알림을 보내는 기능 (영상 전송 버튼 누륵고 잘 도착했다는 HttpStatus.Ok뜨면)
     //일단 알림을 구성하고, 해당 알림에 대한 이벤트를 발생시킴
         // 어떤 회원에게 알림을 보낼지에 대해 찾음
         // 알림을 받을 회원의 emitter를 찾아 해당 Emitter로 Send
-    void send(Member reciever, String content, String url);
+    void send(Long member_to, String content, String url);
 
 }
