f2af5638ce622250addb86202fded54e28460b3c
기남석 <kee0304@naver.com>
2023-02-08 10:26:49 +0900

    [MW] test backup before merge: :

--- RM/MW/mirrorweb/src/pages/QR.js
+++ RM/MW/mirrorweb/src/pages/QR.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { setFamilyAccessToken } from '../modules/valid';
 
 
 function QR() {
