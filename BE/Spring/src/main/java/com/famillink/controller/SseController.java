4f3efce69bc81dc72c01c109dae2e561643dd1be
최다혜 <dhc0404@naver.com>
2023-02-09 09:29:11 +0900

    [BE] feat : SSE 로그아웃 시 emitter 삭제 기능

--- BE/Spring/src/main/java/com/famillink/controller/SseController.java
+++ BE/Spring/src/main/java/com/famillink/controller/SseController.java
@@ -1,6 +1,7 @@
 package com.famillink.controller;
 
 import com.famillink.model.domain.user.Member;
+import com.famillink.model.service.EmitterService;
 import com.famillink.model.service.SseService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,8 @@ public class SseController {
 
     private final SseService sseService;
 
+    private final EmitterService emitterService;
+
     @ApiOperation(value = "알림 구독", notes = "알림을 구독한다.")
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
     @ResponseStatus(HttpStatus.OK)
@@ -24,6 +27,18 @@ public class SseController {
         Member member = (Member) authentication.getPrincipal();
 
         return sseService.subscribe(member.getUid(), lastEventId);
+    }
+
+    @ApiOperation(value = "알림 구독 해제", notes = "알림을 구독을 해제한다.")
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    @ResponseStatus(HttpStatus.OK)
+    public void logout(Long member_uid){
+
+        String member_id = sseService.makeTimeIncludeUid(member_uid);
+
+        emitterService.deleteAllEmitterStartWithMemberUid(member_id);
+        emitterService.deleteAllEventCacheStartWithId(member_id);
+
     }
 
 
