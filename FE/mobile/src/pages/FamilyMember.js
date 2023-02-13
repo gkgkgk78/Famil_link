6b727b933a785e328d206ae75520e61584086372
gemong2 <gemong2@naver.com>
2023-02-14 02:49:53 +0900

    [FE] feat: 가족 프로필

--- FE/mobile/src/pages/FamilyMember.js
+++ FE/mobile/src/pages/FamilyMember.js
@@ -1,47 +1,91 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const StyledImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  margin-bottom: 1.5rem;
+`;
 
 const FamilyMember = () => {
-  const [profile, setProfile] = useState([]);
+  const [profile, setProfile] = useState({});
   const [photoUrls, setPhotoUrls] = useState({});
   const [loading, setLoading] = useState(false);
-  const result = []
+  const [families, setFamilies] = useState({});
+  const result = [];
 
-  const token = localStorage.getItem("faccesstoken").replace(/"/gi, '');
+  const token = localStorage.getItem("faccesstoken").replace(/"/gi, "");
   // const name = "토르"
   useEffect(() => {
-    axios.get('http://i8a208.p.ssafy.io:3000/account/member-list', {
-      headers: {
-        "Authorization": `Bearer ${token}`,
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "http://i8a208.p.ssafy.io:3000/account/member-list",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setProfile(res.data.list);
+        localStorage.setItem("profile", JSON.stringify(res.data.list));
+      } catch (err) {
+        console.log(err);
       }
-    })
-    .then(res => {
-      setProfile(res.data.list);
-      console.log(profile)
-    }).catch(err => {
-      console.log(err)
-    })
-  }, [setProfile]);
+    };
+    console.log(profile);
+    if (localStorage.getItem("profile")) {
+      setProfile(JSON.parse(localStorage.getItem("profile")));
+    } else {
+      fetchData();
+    }
+  }, []);
 
-
-  const requests = profile.map(({ name }) =>
-    axios.get(`http://i8a208.p.ssafy.io:3000/photo/${name}`, {
-      responseType:'blob',
-      headers: {
-            "Authorization": `Bearer ${token}`,
+  const requests = Object.values(profile).map(({ name }) => {
+    if (!photoUrls[name]) {
+      axios
+        .get(`http://i8a208.p.ssafy.io:3000/photo/${name}`, {
+          responseType: "blob",
+          headers: {
+            Authorization: `Bearer ${token}`,
           },
-        }).then(res => {
-          const url = window.URL.createObjectURL(new Blob([res.data], { type: res.headers['content-type'] } ));
-          setPhotoUrls(url)
-        }).catch(err => {
-          console.log(err)
         })
-  )
+        .then((res) => {
+          const url = window.URL.createObjectURL(
+            new Blob([res.data], { type: res.headers["content-type"] })
+          );
+          setPhotoUrls((prevUrls) => ({ ...prevUrls, [name]: url }));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+
+  const handleProfile = (event, name, user_id) => {
+    event.preventDefault();
+    setFamilies({ name, user_id });
+  };
 
   return (
     <div>
-      {requests}
-      {/* <img src={photoUrls} alt="load_fail"></img> */}
+      {Object.values(profile).map(({ name }) => {
+        if (photoUrls[name]) {
+          return (
+            <div key={name}>
+              <StyledImg
+                src={photoUrls[name]}
+                alt={name}
+                onClick={(event) => handleProfile(event, name, profile.user_id)}
+              />
+              <p>{name}</p>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
