9e9a0f19ead4398dafc81ab010ddb52c5b9e34a5
최진우 <cjw_git@naver.com>
2023-02-13 23:20:33 +0900

    [WPF] feat : test

---
+++ WPF/FamilLinkProject/Model/Service/MQTTService.cs
@@ -0,0 +1,95 @@
+﻿using MQTTnet.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet.Diagnostics;
+using MQTTnet.Internal;
+using MQTTnet.Protocol;
+using MQTTnet;
+
+namespace FamilLinkProject.Model.Service
+{
+    public class MQTTService
+    {
+        public static async Task RunAsync()
+        {
+            try
+            {
+                var logger = new MqttNetEventLogger();
+
+                var factory = new MqttFactory(logger);
+                var client = factory.CreateMqttClient();
+                var clientOptions = new MqttClientOptions
+                {
+                    ChannelOptions = new MqttClientTcpOptions
+                    {
+                        Server = "localhost"
+                    }
+                };
+
+                client.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                    Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+                    Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                    Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+                    Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+                    Console.WriteLine();
+
+                    return CompletedTask.Instance;
+                };
+
+                client.ConnectedAsync += async e =>
+                {
+                    Console.WriteLine("### CONNECTED WITH SERVER ###");
+                    await client.SubscribeAsync("#");
+                    Console.WriteLine("### SUBSCRIBED ###");
+                };
+
+                client.DisconnectedAsync += async e =>
+                {
+                    Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    try
+                    {
+                        await client.ConnectAsync(clientOptions);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("### RECONNECTING FAILED ###");
+                    }
+                };
+
+                try
+                {
+                    await client.ConnectAsync(clientOptions);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("### CONNECTING FAILED ###" + Environment.NewLine + exception);
+                }
+
+                Console.WriteLine("### WAITING FOR APPLICATION MESSAGES ###");
+
+                while (true)
+                {
+                    Console.ReadLine();
+
+                    await client.SubscribeAsync("test");
+
+                    var applicationMessage = new MqttApplicationMessageBuilder()
+                        .WithTopic("A/B/C")
+                        .WithPayload("Hello World qwer")
+                        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                        .Build();
+
+                    await client.PublishAsync(applicationMessage);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
---
+++ WPF/FamilLinkProject/ViewModel/ContentBindingModel.cs
@@ -0,0 +1,38 @@
+﻿using FamilLinkProject.View.Page;
+using FamilLinkProject.ViewModel.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FamilLinkProject.ViewModel
+{
+    public class ContentBindingModel : NotifyPropertyChanged
+    {
+        private UserControl _page;
+        public UserControl Page
+        {
+            get
+            {
+                return _page;
+            }
+            set
+            {
+                _page = value;
+                OnPropertyChanged("Page");
+            }
+        }
+
+        private static ContentBindingModel instance;
+
+        public static ContentBindingModel GetInstance()
+        {
+            if (instance == null)
+                instance = new ContentBindingModel();
+
+            return instance;
+        }
+    }
+}
